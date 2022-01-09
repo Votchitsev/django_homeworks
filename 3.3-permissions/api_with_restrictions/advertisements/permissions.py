e0f127d7c7eb7db0113a0ac64615038a141e07ab
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-01-09 20:03:39 +0200

    add admin access

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -3,4 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.creator
+        if request.user == obj.creator:
+            return True
+        elif request.user.is_staff is True:
+            return True
+        else:
+            return False
